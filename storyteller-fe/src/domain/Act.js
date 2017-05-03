a52fb2af2aa5fd6c2980b81727816a273409dec9
Mario Giugno <mariogiugno@gmx.de>
2017-05-03 18:56:24 +0200

    Add domain element events

--- storyteller-fe/src/domain/Act.js
+++ storyteller-fe/src/domain/Act.js
@@ -3,6 +3,17 @@ import {EventEmitter} from 'events';
 import Scene from './Scene';
 import {autobind_functions} from './../utils/autobind_functions';
 
+
+/**
+ * An act is a definite section of a story. One story
+ * can have several acts, and one act is made up of
+ * several scenes
+ * .
+ * Class-Events:
+ * sceneActChanged(act, scene, oldact (this), newAct)
+ * sceneTitleChanged(act, scene, oldTitle, newTitle)
+ * sceneDescriptionChanged(act, scene, oldDescription, newDescription)
+ */
 class Act extends EventEmitter {
 
     uuid = uuidV4();
@@ -22,6 +33,9 @@ class Act extends EventEmitter {
     addScene(uuid) {
         const newScene = new Scene(this);
         newScene.uuid = uuid || newScene.uuid;
+        newScene.on('actChanged', this.onSceneActChanged);
+        newScene.on('titleChanged', this.onSceneTitleChanged);
+        newScene.on('descriptionChanged', this.onSceneDescriptionChanged);
         this.scenes.push(newScene);
         this.emit('sceneAdded', this, newScene);
     }
@@ -31,6 +45,19 @@ class Act extends EventEmitter {
             return scene.uuid !== uuid;
         });
         this.emit('sceneRemoved', this, this.scenes);
+    }
+
+    onSceneActChanged(scene, oldAct, newAct) {
+        // TODO unsubscribe this scene
+        this.emit('sceneActChanged', this, scene, oldAct, newAct);
+    }
+
+    onSceneTitleChanged(oldTitle, newTitle) {
+        this.emit('sceneTitleChanged', this, scene, oldTitle, newTitle);
+    }
+
+    onSceneDescriptionChanged(oldDesc, newDesc) {
+        this.emit('sceneDescriptionChanged', this, scene, oldDesc, newDesc);
     }
 
 }
--- storyteller-fe/src/domain/Scene.js
+++ storyteller-fe/src/domain/Scene.js
@@ -1,14 +1,58 @@
 import uuidV4 from 'uuid/v4';
+import {EventEmitter} from 'events';
 
-class Scene {
+/**
+ * A scene gives the characters a concrete context.
+ * Fachlich, characters move and handeln in a given
+ * context.
+ *
+ * Class-Events:
+ * actChanged(scene, oldAct, newAct)
+ * titleChanged(scene, oldTitle, newTitle)
+ * descriptionChanged(scene, oldDescription, newDescription)
+ */
+class Scene extends EventEmitter {
+
 
     uuid = uuidV4();
-    act;
-    title = '';
-    description = '';
+    _act;
+    _title = '';
+    _description = '';
 
-    constructor(act) {
-        this.act = act;
+    get act() {
+        return this._act;
+    }
+
+    set act(act) {
+        const oldAct = this._act;
+        this._act = act;
+        this.emit('actChanged', this, oldAct, act);
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title) {
+        const oldTitle = this._title;
+        this._title = title;
+        this.emit('titleChanged', this, oldTitle, title);
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(description) {
+        const oldDescription = this._description;
+        this._description = description;
+        this.emit('descriptionChanged', this, oldDescription, description);
+    }
+
+    constructor(act, title = '', desciption = '') {
+        this._act = act;
+        this._title = title;
+        this._description = desciption;
     }
 
 }
